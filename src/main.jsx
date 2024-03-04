e76c174c4cca8c6da5c7035bee37f38606149d11
miquelven <miquelven.silva@gmail.com>
2024-03-04 15:35:38 -0300

    adição do react-router-dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,28 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import ReactDOM from "react-dom";
+import App from "./pages/App.jsx";
+import Modal from "./pages/Modal.jsx";
+
 import "./index.css";
 import "./App.css";
 
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/:idImage",
+        element: <Modal />,
+      },
+    ],
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
