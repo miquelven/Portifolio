b7c4fd270892b84054f66369610f476d9fab514e
miquelven <miquelven.silva@gmail.com>
2024-03-06 14:14:56 -0300

    remoção da animação

--- src/assets/components/Skills/index.jsx
+++ src/assets/components/Skills/index.jsx
@@ -1,6 +1,3 @@
-import { motion } from "framer-motion";
-import { useEffect, useRef, useState } from "react";
-
 import javascript from "../../javascript.png";
 import typescript from "../../typescript.png";
 import nuxt from "../../nuxt.png";
@@ -10,93 +7,57 @@ import vue from "../../vue.png";
 import react from "../../react.png";
 
 export default function Skills() {
+  const timer = 200;
+
   return (
     <div className="h-96 pr-7 flex-1" data-aos="zoom-in" data-aos-delay="500">
-      <ShuffleGrid />;
+      <div className="grid grid-cols-2 grid-rows-4 gap-8 ">
+        {squareData.map((square) => (
+          <div
+            key={square.id}
+            data-aos="fade-left"
+            data-aos-delay={timer * square.id}
+            className="flex justify-center items-center max-h-20 rounded-lg shadow-md shadow-[#306d9d]"
+          >
+            <img
+              src={square.src}
+              alt="Imagem da tecnologia"
+              className="w-3/4 object-cover "
+            />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
 
-const shuffle = (array) => {
-  let currentIndex = array.length,
-    randomIndex;
-
-  while (currentIndex != 0) {
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex--;
-
-    [array[currentIndex], array[randomIndex]] = [
-      array[randomIndex],
-      array[currentIndex],
-    ];
-  }
-
-  return array;
-};
-
 const squareData = [
   {
     id: 1,
-    src: javascript,
-  },
-  {
-    id: 2,
-    src: typescript,
-  },
-  {
-    id: 3,
     src: nuxt,
   },
   {
-    id: 4,
-    src: tailwindcss,
-  },
-  {
-    id: 5,
-    src: Sass,
-  },
-  {
-    id: 6,
+    id: 2,
     src: vue,
   },
   {
-    id: 7,
+    id: 3,
     src: react,
   },
+  {
+    id: 4,
+    src: javascript,
+  },
+  {
+    id: 5,
+    src: typescript,
+  },
+  {
+    id: 6,
+    src: tailwindcss,
+  },
+  {
+    id: 7,
+    src: Sass,
+  },
 ];
-
-const generateSquares = () => {
-  return shuffle(squareData).map((sq) => (
-    <motion.div
-      key={sq.id}
-      layout
-      transition={{ duration: 1.5, type: "spring" }}
-      className="flex items-center justify-center h-[70px] w-[170px] shadow-md shadow-[#245276] rounded-xl"
-    >
-      <img className="object-cover w-36" src={sq.src}></img>
-    </motion.div>
-  ));
-};
-
-const ShuffleGrid = () => {
-  const timeoutRef = useRef(null);
-  const [squares, setSquares] = useState(generateSquares());
-
-  useEffect(() => {
-    shuffleSquares();
-
-    return () => clearTimeout(timeoutRef.current);
-  }, []);
-
-  const shuffleSquares = () => {
-    setSquares(generateSquares());
-
-    timeoutRef.current = setTimeout(shuffleSquares, 3000);
-  };
-
-  return (
-    <div className="grid   grid-cols-2 grid-rows-4 h-[7s50px] gap-10 ">
-      {squares.map((sq) => sq)}
-    </div>
-  );
-};
