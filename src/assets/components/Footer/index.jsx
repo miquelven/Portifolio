7cb052e9fdd6a47db3a9c40bcf17072492a6d9e0
miquelven <miquelven.silva@gmail.com>
2024-03-06 21:56:51 -0300

    adição da responsividade

--- src/assets/components/Footer/index.jsx
+++ src/assets/components/Footer/index.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="bg-[#245276] w-full h-16 flex justify-center items-center text-white">
-      <p>
+      <p className="max-sm:text-sm">
         Desenvolvido por <span className="text-[#3c88c4]"> Miquelven</span>
       </p>
     </footer>
