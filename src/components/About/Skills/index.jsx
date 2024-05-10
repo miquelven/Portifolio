fd74c21cfd98b3fc956322b151f02357f60a35ca
miquelven <miquelven.silva@gmail.com>
2024-05-10 15:49:24 -0300

    refactor: remove data aos fade left

--- src/components/About/Skills/index.jsx
+++ src/components/About/Skills/index.jsx
@@ -4,12 +4,12 @@ export default function Skills() {
   const timer = 200;
 
   return (
-    <div className="h-96 pr-7 flex-1" data-aos="fade-left" data-aos-delay="600">
+    <div className="h-96 pr-7 flex-1">
       <div className="grid grid-cols-2 grid-rows-4 gap-8 max-xl:grid-cols-4 max-lg:grid-cols-3 max-sm:grid-cols-2 max-[530px]:grid-cols-1">
         {skills.map((skill) => (
           <div
             key={skill.id}
-            data-aos="fade-left"
+            data-aos="zoom-in"
             data-aos-delay={timer * skill.id}
             className="flex justify-center items-center max-h-20 w-52 rounded-lg shadow-md shadow-[#306d9d] max-[530px]:w-full"
           >
