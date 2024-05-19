5817fab95a889d461c22a226ea44e9ce953cff9e
miquelven <miquelven.silva@gmail.com>
2024-05-18 21:34:12 -0300

    style: change the footer background color

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="bg-[#245276] w-full h-16 flex justify-center items-center text-white">
+    <footer className="bg-[#3c88c4] w-full h-16 flex justify-center items-center text-white">
       <p className="max-sm:text-sm">Desenvolvido por Miquelven</p>
     </footer>
   );
