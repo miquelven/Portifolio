09abbf3cd6b6ef61e8b071f3fe10fa52fcf453e8
miquelven <miquelven.silva@gmail.com>
2024-05-23 14:44:29 -0300

    refactor: change in attributes for test file

--- src/components/DownloadButton/index.jsx
+++ src/components/DownloadButton/index.jsx
@@ -10,27 +10,40 @@ export default function DownloadButton() {
   const downloadText = useRef(null);
 
   const animationEnd = () => {
-    console.log("oi");
-    downloadLoader.current.setAttribute(`class`, "hidden");
-    downloadCheckMark.current.removeAttribute(`class`);
-    downloadText.current.innerHTML = "Baixado";
-    setTimeout(() => {
-      downloadText.current.innerHTML = "Currículo";
-      downloadCheckMark.current.setAttribute(`class`, "hidden");
-      downloadIcon.current.removeAttribute(`class`);
-    }, [1000]);
+    if (
+      downloadLoader.current &&
+      downloadCheckMark.current &&
+      downloadText.current &&
+      downloadIcon.current
+    ) {
+      downloadLoader.current.classList.add("hidden");
+      downloadCheckMark.current.classList.remove("hidden");
+      downloadText.current.innerHTML = "Baixado";
+      setTimeout(() => {
+        downloadText.current.innerHTML = "Currículo";
+        downloadCheckMark.current.classList.add("hidden");
+        downloadIcon.current.classList.remove("hidden");
+      }, 1000);
+    }
   };
 
   const animationStart = () => {
-    downloadIcon.current.setAttribute(`class`, "hidden");
-    downloadLoader.current.removeAttribute(`class`);
-    downloadText.current.innerHTML = "Baixando";
-    setTimeout(() => animationEnd(), [1000]);
+    if (
+      downloadIcon.current &&
+      downloadLoader.current &&
+      downloadText.current
+    ) {
+      downloadIcon.current.classList.add("hidden");
+      downloadLoader.current.classList.remove("hidden");
+      downloadText.current.innerHTML = "Baixando";
+      setTimeout(animationEnd, 1000);
+    }
   };
 
   return (
     <div className="flex justify-center absolute w-1/2 m-auto shadow-sm transition-all duration-300 border-2 border-[#245276]  bottom-4 left-4 right-4 z-10 rounded-lg  bg-white  text-[#245276] backdrop-blur font-bold hover:shadow-[#3c88c4] hover:-translate-x-[1px] hover:-translate-y-[5px]">
       <a
+        role="link"
         href={downloadCV}
         download
         target="_blank"
@@ -40,6 +53,7 @@ export default function DownloadButton() {
         <div className="flex justify-center items-center relative">
           <div className="svg-container">
             <svg
+              role="img"
               ref={downloadIcon}
               width="18"
               height="22"
@@ -51,26 +65,28 @@ export default function DownloadButton() {
                 className="download-arrow"
                 d="M13 9L9 13M9 13L5 9M9 13V1"
                 stroke="#245276"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
               />
               <path
                 d="M1 17V18C1 18.7956 1.31607 19.5587 1.87868 20.1213C2.44129 20.6839 3.20435 21 4 21H14C14.7956 21 15.5587 20.6839 16.1213 20.1213C16.6839 19.5587 17 18.7956 17 18V17"
                 stroke="#245276"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
               />
             </svg>
             <div
+              data-testid="download-loader"
               ref={downloadLoader}
               id="download-loader"
               className="text-[#245276] hidden"
             ></div>
             <svg
+              data-testid="checkmark"
               ref={downloadCheckMark}
-              className=" hidden"
+              className="hidden"
               width="20"
               height="20"
               viewBox="0 0 20 20"
@@ -78,8 +94,8 @@ export default function DownloadButton() {
               xmlns="http://www.w3.org/2000/svg"
             >
               <path
-                fill-rule="evenodd"
-                clip-rule="evenodd"
+                fillRule="evenodd"
+                clipRule="evenodd"
                 d="M10 20C15.5228 20 20 15.5228 20 10C20 4.47715 15.5228 0 10 0C4.47715 0 0 4.47715 0 10C0 15.5228 4.47715 20 10 20ZM15.1071 7.9071C15.4976 7.51658 15.4976 6.88341 15.1071 6.49289C14.7165 6.10237 14.0834 6.10237 13.6929 6.49289L8.68568 11.5001L7.10707 9.92146C6.71655 9.53094 6.08338 9.53094 5.69286 9.92146C5.30233 10.312 5.30233 10.9452 5.69286 11.3357L7.97857 13.6214C8.3691 14.0119 9.00226 14.0119 9.39279 13.6214L15.1071 7.9071Z"
                 fill="#245276"
               />
