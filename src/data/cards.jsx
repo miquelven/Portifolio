f6dff63d4fbef896ad0897a4e94ec4774af2ba5a
miquelven <miquelven.silva@gmail.com>
2024-07-02 17:18:47 -0300

    refactor: change text content

--- src/data/cards.jsx
+++ src/data/cards.jsx
@@ -22,7 +22,7 @@ const cards = [
     url: game,
     title: "MiGame",
     description:
-      "O miGame possui um sistema de classificação competitivo, onde as pontuações mais altas dos jogadores são registradas e classificadas entre as melhores de todas. Ao desenvolver o miGame, concentrei-me em criar uma interface amigável e intuitiva que proporcionasse uma experiência de usuário fluida e agradável. Além disso, implementei medidas de segurança robustas para garantir a proteção dos dados dos usuários, especialmente no que diz respeito ao processo de login e registro.",
+      "Desenvolvi o miGame com um sistema de classificação competitivo que registra as pontuações mais altas. Foquei em criar uma interface amigável e intuitiva para uma experiência fluida. Implementações de segurança robustas garantem a proteção dos dados dos usuários, especialmente no login e registro.",
     stacks: [
       <FaVuejs />,
       <SiTailwindcss />,
@@ -40,7 +40,7 @@ const cards = [
     url: miCoin,
     title: "MiCoin",
     description:
-      " Com uma variedade de recursos intuitivos, o miCoin permite que você acompanhe informações detalhadas sobre várias moedas digitais. Favoritos personalizados permitem que você mantenha suas escolhas em destaque, enquanto o monitoramento de preços por e-mail o mantém informado sobre as mudanças do mercado. Com recursos de filtragem por categorias e opções de paginação, você tem controle total sobre sua experiência de navegação. Explore, analise e acompanhe as criptomoedas de forma conveniente e eficaz com o miCoin.",
+      "O miCoin oferece recursos intuitivos para acompanhar informações de várias moedas digitais. Favoritos personalizados e monitoramento de preços por e-mail mantêm você informado sobre o mercado. Com filtragem por categorias e paginação, você tem controle total sobre a navegação e análise das criptomoedas.",
     stacks: [<FaReact />, <SiTailwindcss />, <IoLogoJavascript />],
     stackColors: ["#087ea4", "#1dc0cd", "#d6ba32"],
     id: simpleFaker.string.uuid(),
@@ -51,7 +51,7 @@ const cards = [
     url: miMovie,
     title: "MiMovie",
     description:
-      "MiMovie é sua plataforma completa para explorar vários filmes. Com uma extensa biblioteca de informações. Com recursos de pesquisa avançada, você pode facilmente encontrar o que procura. Além disso, os filtros por categoria permitem que você refine sua busca. Além de poder favoritar os filmes e salvar para assistir mais tarde.",
+      "MiMovie é sua plataforma completa para explorar filmes. Com uma extensa biblioteca e pesquisa avançada, encontre facilmente o que procura. Use filtros por categoria, favorite filmes e salve para assistir mais tarde.",
     stacks: [<FaReact />, <SiTailwindcss />, <SiTypescript />],
     stackColors: ["#087ea4", "#1dc0cd", "#087ecf"],
     id: simpleFaker.string.uuid(),
@@ -63,7 +63,7 @@ const cards = [
     url: miDrinks,
     title: "MiDrinks",
     description:
-      "MiDrinks é sua plataforma completa para explorar o vasto universo das bebidas. Com uma extensa biblioteca de informações. Com recursos de pesquisa avançada, você pode facilmente encontrar o que procura, seja um coquetel específico ou uma bebida com ingredientes específicos. Além disso, os filtros por categoria permitem que você refine sua busca, seja por tipo de bebida alcoólica, não alcoólica ou sazonal.",
+      "MiDrinks é sua plataforma para explorar bebidas. Com uma vasta biblioteca e pesquisa avançada, encontre coquetéis ou bebidas específicas. Filtros por categoria ajudam a refinar sua busca, seja por tipo de bebida alcoólica, não alcoólica ou sazonal.",
     stacks: [<SiNuxtdotjs />, <FaSass />, <SiTypescript />],
     stackColors: ["#0ade87", "#ce6b9c", "#087ecf"],
     id: simpleFaker.string.uuid(),
@@ -75,7 +75,7 @@ const cards = [
     url: MiRestaurante,
     title: "MiRestaurante",
     description:
-      "MiRestaurante foi meticulosamente projetado para apresentar um espaço gastronômico fictício, onde cada detalhe foi pensado para proporcionar uma experiência única aos nossos visitantes. Com um design simples e objetivo buscando cativar novos clientes. Ressaltando alguns pratos, avaliações de clientes, endereço e tambem o seu ambiente acolhedor.",
+      "MiRestaurante é um espaço gastronômico fictício, projetado para cativar clientes com design simples e objetivo. Destaca pratos, avaliações de clientes, endereço e um ambiente acolhedor.",
     stacks: [<FaReact />, <SiTailwindcss />, <IoLogoJavascript />],
     stackColors: ["#087ea4", "#1dc0cd", "#d6ba32"],
     id: simpleFaker.string.uuid(),
@@ -87,7 +87,7 @@ const cards = [
     url: MiEstilo,
     title: "MiEstilo",
     description:
-      "MiEstilo é uma Landing Page que oferece uma ampla seleção de produtos, coleções exclusivas e descontos irresistíveis. Nossa abordagem é direta e objetiva, com o objetivo de cativar novos clientes e proporcionar uma experiência visualmente agradável. ",
+      "MiEstilo é uma landing page que oferece uma ampla seleção de produtos, coleções exclusivas e descontos. Com uma abordagem direta e objetiva, busca cativar novos clientes e proporcionar uma experiência visualmente agradável.",
     stacks: [<FaVuejs />, <SiTailwindcss />, <IoLogoJavascript />],
     stackColors: ["#6cb485", "#1dc0cd", "#d6ba32"],
     id: simpleFaker.string.uuid(),
