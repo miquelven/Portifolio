ee775d492d4b5f8143ab357b486dc603b19bcdd7
miquelven <miquelven.silva@gmail.com>
2024-07-04 20:02:16 -0300

    refactor: change the content

--- postcss.config.js
+++ postcss.config.js
@@ -1,16 +1,6 @@
-import purgecss from "@fullhuman/postcss-purgecss";
-import tailwindcss from "tailwindcss";
-import autoprefixer from "autoprefixer";
-
-const purgecssPlugin = purgecss({
-  content: ["./public/**/*.html", "./src/**/*.react"],
-  defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-});
-
 export default {
-  plugins: [
-    tailwindcss,
-    autoprefixer,
-    ...(process.env.NODE_ENV === "production" ? [purgecssPlugin] : []),
-  ],
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 };
