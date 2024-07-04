5715a7d6e0d6af8c6684c5a29c766a514cbebe92
miquelven <miquelven.silva@gmail.com>
2024-07-04 19:54:41 -0300

    feat: add config postcss

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,16 @@
+import purgecss from "@fullhuman/postcss-purgecss";
+import tailwindcss from "tailwindcss";
+import autoprefixer from "autoprefixer";
+
+const purgecssPlugin = purgecss({
+  content: ["./public/**/*.html", "./src/**/*.vue"],
+  defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+});
+
 export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+  plugins: [
+    tailwindcss,
+    autoprefixer,
+    ...(process.env.NODE_ENV === "production" ? [purgecssPlugin] : []),
+  ],
+};
