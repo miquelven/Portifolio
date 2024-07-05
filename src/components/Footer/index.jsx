7c430410707d2389413405165182799209063ce5
miquelven <miquelven.silva@gmail.com>
2024-07-04 22:13:33 -0300

    style: change font weight for name in footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,7 +1,10 @@
 export default function Footer() {
   return (
     <footer className="bg-[#3c88c4] w-full h-16 flex justify-center items-center text-white">
-      <p className="max-sm:text-sm">Desenvolvido por Miquelven</p>
+      <p className="max-sm:text-sm">
+        Desenvolvido por
+        <span className=" font-bold"> Miquelven</span>
+      </p>
     </footer>
   );
 }
