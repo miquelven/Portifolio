08b99d719f8c606728d01558d3ddb5c9ac69212e
miquelven <miquelven.silva@gmail.com>
2025-08-16 16:05:19 -0300

    feat: adicao do nome da empresa no title da xp

--- src/components/Experience/index.jsx
+++ src/components/Experience/index.jsx
@@ -2,7 +2,8 @@ export default function Experience() {
   const experiences = [
     {
       id: 1,
-      title: "Desenvolvedor Front End",
+      title: "Desenvolvedor Front End - ",
+      company: "Guichê Web",
 
       description:
         "Atualmente, sou responsável pela criação do novo site da empresa, focando em Next.js, TypeScript, JavaScript e Tailwind para entregar interfaces modernas, responsivas e de alta performance. Minha atuação envolve desde a implementação das telas até a colaboração com o time de design no Figma, onde contribuo com sugestões de melhorias e ajustes. Além disso, permaneço em constante interação com outras equipes, pronto para dar manutenção em futuros projetos e assegurar a melhor experiência possível para os usuários.",
@@ -30,6 +31,7 @@ export default function Experience() {
             <div className="flex justify-between items-start mb-4">
               <h3 className="text-2xl font-bold text-white group-hover:text-red-400 transition-colors duration-300">
                 {exp.title}
+                {exp.company}
               </h3>
               <div className="w-12 h-12 bg-gradient-to-r from-red-500 to-red-600 rounded-full flex items-center justify-center text-white font-bold group-hover:scale-110 transition-transform duration-300">
                 {exp.icon}
